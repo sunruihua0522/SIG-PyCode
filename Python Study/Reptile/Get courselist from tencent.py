3454a06e76868c80f068777dfdfaf5c1b712fe37
Ruihua.Sun <--global>
2019-09-23 15:45:44 +0800

    一定要找到目标元素特有的特性，将其选择出来

--- Python Study/Reptile/Get courselist from tencent.py
+++ Python Study/Reptile/Get courselist from tencent.py
@@ -1,17 +1,17 @@
 import requests
-import os
+import xlwt
 from bs4 import BeautifulSoup as bs
 import re
 
 '''获取指定页面之间的课程列表'''
 def GetCourses(StartPage, EndPage):
     cs=[]
-    for i in range(StartPage,EndPage+1):
+    for i in range(StartPage,EndPage):
         txturl = 'https://ke.qq.com/course/list?mt=1001&st=2002&page='+str(i)
         resp = requests.get(txturl)
         txtBs = bs(resp.text,'html.parser')
         list = txtBs.find_all('li',{'class':'course-card-item'})
-        matchTxt='class="item-status"'
+        matchTxt='item-line item-line--middle'
         count=0
         for l in list:
             g = re.findall(matchTxt,str(l))
@@ -21,7 +21,12 @@ def GetCourses(StartPage, EndPage):
     return cs
 
 if __name__=="__main__":
-    Courses = GetCourses(1,50)
+    Courses = GetCourses(51,101)
+    book = xlwt.Workbook()
+    newSheet = book.add_sheet('Courses')
+    row = 0
     for c in Courses:
-        print(c)
+        newSheet.write(row,0,str(c))
+        row = row+1
+    book.save('All courses50-100.xlsx')
     print(len(Courses))
