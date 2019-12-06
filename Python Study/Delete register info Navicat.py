343ff223fbe5339bfd938e1a7155f3754c7be4c2
Ruihua.Sun <--global>
2019-12-06 13:51:05 +0800

    解决删除注册表子项无法删除的问题，需要先删除子项再删除父项

--- Python Study/Delete register info Navicat.py
+++ Python Study/Delete register info Navicat.py
@@ -1,18 +1,38 @@
 import winreg
 
-#删除HKEY_CURRENT_USER\SOFTWARE\PremiumSoft
-key = winreg.OpenKey(key = winreg.HKEY_CURRENT_USER,sub_key = 'Software\PremiumSoft', access = winreg.KEY_ALL_ACCESS)
-try:
-    clsid = 0
-    while(1):
-        x = winreg.EnumKey(key,clsid)
-        print('remiumSoft的子键',x)
-        if(x.lower()=='data'):
-            winreg.DeleteKey(key, 'Data')
-        clsid += 1
-except Exception as e:
-    print(e)
+def deleteSubkey(key0, key1, key2=""):
+    if key2=="":
+        currentkey = key1
+    else:
+        currentkey = key1+ "\\" +key2
 
+    open_key = winreg.OpenKey(key0, currentkey ,0,winreg.KEY_ALL_ACCESS)
+    infokey = winreg.QueryInfoKey(open_key)
+    for x in range(0, infokey[0]):
+        #NOTE:: This code is to delete the key and all subkeys.
+        #  If you just want to walk through them, then
+        #  you should pass x to EnumKey. subkey = _winreg.EnumKey(open_key, x)
+        #  Deleting the subkey will change the SubKey count used by EnumKey.
+        #  We must always pass 0 to EnumKey so we
+        #  always get back the new first SubKey.
+        subkey = winreg.EnumKey(open_key, 0)
+        try:
+            winreg.DeleteKey(open_key, subkey)
+            print ("Removed %s\\%s " % ( currentkey, subkey))
+        except:
+            deleteSubkey( key0, currentkey, subkey )
+            # no extra delete here since each call
+            #to deleteSubkey will try to delete itself when its empty.
+
+    winreg.DeleteKey(open_key,"")
+    open_key.Close()
+    print ("Removed %s" % (currentkey))
+    return
+
+#删除HKEY_CURRENT_USER\SOFTWARE\PremiumSoft
+#key = winreg.OpenKey(key = winreg.HKEY_CURRENT_USER,sub_key = 'Software\PremiumSoft', access = winreg.KEY_ALL_ACCESS)
+
+deleteSubkey(winreg.HKEY_CURRENT_USER,'Software\PremiumSoft','Data')
 
 
 #删除所有的Info子键
