483a67bf679f54ab7405c2362d9cfe47daa2bc0f
Ruihua.Sun <--global>
2020-04-02 12:37:02 +0800

    实现了flask长连接的功能

--- Web/TestLongConnectionWithServer/main.py
+++ Web/TestLongConnectionWithServer/main.py
@@ -1,25 +1,28 @@
-from flask import Flask,request,render_template
 from geventwebsocket.handler import WebSocketHandler
-from geventwebsocket.server import WSGIServer
-from geventwebsocket.websocket import WebSocket
-
+from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template
+import datetime,time
 app = Flask(__name__)
 
-app.route('/',methods=['POST','GET'])
+@app.route('/')
 def index():
-    client_socket = request.environ.get('wsgi.websocket')
-
-    while True:
-        #msg = client_socket.recive()
-
-        msg = client_socket.receive()
-        client_socket.send(msg + 'from web service')
-
-@app.route('/chart')
-def chart():
     return render_template('client.html')
-if __name__ == '__main__':
-    http_server = WSGIServer(('127.0.0.1',8888),application=app,
-                             handler_class=WebSocketHandler)
-    http_server.serve_forever()
 
+@app.route('/api')
+def api():
+    if request.environ.get('wsgi.websocket'):
+        ws = request.environ['wsgi.websocket']
+        orgin =  ws.origin
+        print('%s加入连接'%orgin)
+        while True:
+            message = ws.receive()
+            for _ in range(int(message)):
+                time.sleep(0.5)
+                msg = '%s:  %s'%(datetime.datetime.now().__str__(), message)
+                ws.send(msg)
+
+    return
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('127.0.0.1',5000), application= app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
